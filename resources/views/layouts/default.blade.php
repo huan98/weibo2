6a7d6300b8687e6316cbdda6bd1763617bab6458
huan98 <847693739@qq.com>
2020-01-22 13:12:42 +0800

    用户注册表单

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,7 +8,7 @@
     @include('layouts._header')
 
     <div class="container">
-      <div class="offset-ma-1 col-md-10">
+      <div class="offset-md-1 col-md-10">
         @yield('content')
         @include('layouts._footer')
       </div>
