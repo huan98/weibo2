93ecff0c4139ef9e7fa449d4df8c0fdd777cf967
huan98 <847693739@qq.com>
2020-05-25 16:29:16 +0800

    Updated database configuration2

--- config/database.php
+++ config/database.php
@@ -67,7 +67,6 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
             'host' => $db_config['host'],
             'port' => env('DB_PORT', '5432'),
             'database' => $db_config['database'],
